e4d2f006653bda5e55306c4920da932ddf4451ba
Nicola Febbrari <febbrari@gmail.com>
2017-06-03 15:13:46 +0200

    doc

--- src/prjTriangleNumericUndir/Reducer2.java
+++ src/prjTriangleNumericUndir/Reducer2.java
@@ -9,36 +9,31 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class Reducer2 extends Reducer<LongLongBit, LongWritable, Text, Text> {
-	private Text outText = new Text();
-	//private List<Long> vals = new LinkedList<Long>();
+    private Text outText = new Text();
+    //private List<Long> vals = new LinkedList<Long>();
 
-	@Override
-	protected void reduce(LongLongBit key, Iterable<LongWritable> values,
-			Context context) throws IOException, InterruptedException {
-		boolean ok = false;
-		//vals.clear();
-		Boolean k = key.getTer().get();
-		for (LongWritable val : values) {
-			k = key.getTer().get();//forse da togliere
-			if (!k) {
-				ok = true;
-			} else {
-				if (ok)
-					WriteContext(val.get(),key.getFirst().get(), key.getSecond().get(), context);
-				else
-					return;
-			}
-		}
-	}
+    @Override
+    protected void reduce(LongLongBit key, Iterable<LongWritable> values,
+                          Context context) throws IOException, InterruptedException {
+        boolean ok = false;
+        //vals.clear();
+        Boolean k = key.getTer().get();
+        if (!k) {
+            for (LongWritable val : values) {
+                if (val.get() >0) // && val>0
+                    WriteContext(val.get(), key.getFirst().get(), key.getSecond().get(), context);
+            }
+        }
+    }
 
-	private void WriteContext(Long first, Long second, Long ter,
-			Context context) throws IOException, InterruptedException {
-		// per evitare duplicazioni
+    private void WriteContext(Long first, Long second, Long ter,
+                              Context context) throws IOException, InterruptedException {
+        // per evitare duplicazioni
 
-			outText.set(first + " " + second + " " + ter);
-			context.write(outText, new Text());
-			//vals.add(second);
-			return;
+        outText.set(first + " " + second + " " + ter);
+        context.write(outText, new Text());
+        //vals.add(second);
+        return;
 
-	}
+    }
 }
